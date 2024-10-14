24c56686f75ab42c3a0d21a9a9805a1f3a3d8f4b
patrykwsiz <w66024@student.wsiz.edu.pl>
2024-10-14 21:42:23 +0200

    strona główna, download apk

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('welcome');
+Route::get('/download-apk', function () {
+    $file = public_path('apk/app-release.apk');
+    return Response::download($file);
 });
